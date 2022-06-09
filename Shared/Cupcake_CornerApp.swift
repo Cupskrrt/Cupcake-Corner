8ba49ac121a8a6f3a2f6d3b940d78d74c855e736
Cupskrrt <khanalfarizzi@hotmail.com>
2022-06-09 19:44:32 +0700

    Initial Commit

---
+++ Shared/Cupcake_CornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cupcake_CornerApp.swift
+//  Shared
+//
+//  Created by Luthfi Khan on 09/06/22.
+//
+
+import SwiftUI
+
+@main
+struct Cupcake_CornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
